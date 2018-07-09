f19801a0de499449e1ee77cf8df4d258da10e10d
danmj <daniel.jung@stern.nyu.edu>
2018-07-09 20:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
-  const factoid = []
-  const i = 0
+  var factoid = []
+  var i = 0
   while (i < facts.length) {
     factoid.push(facts[i] + "!!!")
    i++
